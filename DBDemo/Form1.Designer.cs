e13827c002b2150f3566daf107e9338cad75204a
Thomas Frenzel <t.frentzel@gmx.de>
2018-06-20 11:43:03 +0200

    Update

---
+++ DBDemo/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace DBDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chantalDataSet = new DBDemo.chantalDataSet();
+            this.adressenBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.adressenTableAdapter = new DBDemo.chantalDataSetTableAdapters.adressenTableAdapter();
+            this.it_persoenlichkeitenDataSet = new DBDemo.it_persoenlichkeitenDataSet();
+            this.personenBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.personenTableAdapter = new DBDemo.it_persoenlichkeitenDataSetTableAdapters.personenTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.db_buecherDataSet = new DBDemo.db_buecherDataSet();
+            this.buecherBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buecherTableAdapter = new DBDemo.db_buecherDataSetTableAdapters.buecherTableAdapter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chantalDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adressenBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.it_persoenlichkeitenDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personenBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_buecherDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buecherBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 169);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Daten laden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(442, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Anzeige";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "- leer -";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(683, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(683, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(683, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(683, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(683, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(564, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Name";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(564, 50);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Vorname";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(564, 73);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "Straße";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(564, 96);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(88, 17);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "Hausnummer";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(564, 118);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(46, 17);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.Text = "PLZ";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(301, 169);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 253);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // chantalDataSet
+            // 
+            this.chantalDataSet.DataSetName = "chantalDataSet";
+            this.chantalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // adressenBindingSource
+            // 
+            this.adressenBindingSource.DataMember = "adressen";
+            this.adressenBindingSource.DataSource = this.chantalDataSet;
+            // 
+            // adressenTableAdapter
+            // 
+            this.adressenTableAdapter.ClearBeforeFill = true;
+            // 
+            // it_persoenlichkeitenDataSet
+            // 
+            this.it_persoenlichkeitenDataSet.DataSetName = "it_persoenlichkeitenDataSet";
+            this.it_persoenlichkeitenDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personenBindingSource
+            // 
+            this.personenBindingSource.DataMember = "personen";
+            this.personenBindingSource.DataSource = this.it_persoenlichkeitenDataSet;
+            // 
+            // personenTableAdapter
+            // 
+            this.personenTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(770, 169);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(460, 253);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // db_buecherDataSet
+            // 
+            this.db_buecherDataSet.DataSetName = "db_buecherDataSet";
+            this.db_buecherDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // buecherBindingSource
+            // 
+            this.buecherBindingSource.DataMember = "buecher";
+            this.buecherBindingSource.DataSource = this.db_buecherDataSet;
+            // 
+            // buecherTableAdapter
+            // 
+            this.buecherTableAdapter.ClearBeforeFill = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(770, 467);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(301, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "IT-Persönlichkeiten";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(767, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Bücher";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 725);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chantalDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adressenBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.it_persoenlichkeitenDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personenBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_buecherDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.buecherBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private chantalDataSet chantalDataSet;
+        private System.Windows.Forms.BindingSource adressenBindingSource;
+        private chantalDataSetTableAdapters.adressenTableAdapter adressenTableAdapter;
+        private it_persoenlichkeitenDataSet it_persoenlichkeitenDataSet;
+        private System.Windows.Forms.BindingSource personenBindingSource;
+        private it_persoenlichkeitenDataSetTableAdapters.personenTableAdapter personenTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private db_buecherDataSet db_buecherDataSet;
+        private System.Windows.Forms.BindingSource buecherBindingSource;
+        private db_buecherDataSetTableAdapters.buecherTableAdapter buecherTableAdapter;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ DBDemo/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DBDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string myConnectionString = "SERVER=localhost;" +
+                            "DATABASE=chantal;" +
+                            "UID=root;" +
+                            "PASSWORD=; SslMode=none";
+
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM adressen";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                    row += Reader.GetValue(i).ToString() + ", ";
+                listBox1.Items.Add(row);
+            }
+            connection.Close();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //label1.Text=listBox1.SelectedItem.ToString();
+            string str = listBox1.SelectedItem.ToString();
+            string[] strArray = str.Split(',');
+
+            //var linqQuery = from entry in strArray
+            //                where strArray.I
+            //                select number;
+
+
+            label1.Text = strArray[0];
+
+            label2.Text = (checkBox1.Checked) ? strArray[1] : "";
+            label3.Text = (checkBox2.Checked) ? strArray[2] : "";
+            label4.Text = (checkBox3.Checked) ? strArray[3] : "";
+            label5.Text = (checkBox4.Checked) ? strArray[4] : "";
+            label6.Text = (checkBox5.Checked) ? strArray[5] : "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "db_buecherDataSet.buecher". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.buecherTableAdapter.Fill(this.db_buecherDataSet.buecher);
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "it_persoenlichkeitenDataSet.personen". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.personenTableAdapter.Fill(this.it_persoenlichkeitenDataSet.personen);
+            // TODO: Diese Codezeile lädt Daten in die Tabelle "chantalDataSet.adressen". Sie können sie bei Bedarf verschieben oder entfernen.
+            this.adressenTableAdapter.Fill(this.chantalDataSet.adressen);
+            dataGridView1.DataSource = this.it_persoenlichkeitenDataSet;
+            dataGridView1.DataMember = this.it_persoenlichkeitenDataSet.Tables[0].TableName;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView2.DataSource = this.db_buecherDataSet;
+            dataGridView2.DataMember = this.db_buecherDataSet.Tables[0].TableName;
+            dataGridView2.RowHeadersVisible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            MySqlCommandBuilder commandBuilderPerson = new MySqlCommandBuilder(this.personenTableAdapter.Adapter);          
+            this.personenTableAdapter.Adapter.Update(this.it_persoenlichkeitenDataSet.Tables[0]);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MySqlCommandBuilder commandBuilderBuch = new MySqlCommandBuilder(this.buecherTableAdapter.Adapter);
+            this.buecherTableAdapter.Adapter.Update(this.db_buecherDataSet.Tables[0]);
+        }
+    }
+}
